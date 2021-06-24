323653e6044d1aeeb384c7f6f6b2321cf1dec273
Uncle Button <UncleButton@users.noreply.github.com>
2021-06-23 22:28:27 -0400

    spin multiplier added to in game settings page
    fixed multiplayer issues

---
+++ Assets/SliderUpdateDroneScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderUpdateDroneScript : MonoBehaviour
+{
+    public GameObject drone;
+    public Text spinText;
+
+    private FlightController flightController = null;
+    private FlightControllerSinglePlayer flightControllerSinglePlayer = null;
+
+    private void Start()
+    {
+        if (drone.GetComponent<FlightController>() != null)
+            flightController = drone.GetComponent<FlightController>();
+        else
+            flightControllerSinglePlayer = drone.GetComponent<FlightControllerSinglePlayer>();
+    }
+
+    public void UpdateMultiplier()
+    {
+        if (flightController != null)
+            flightController.spinMultiplier = GetComponent<Slider>().value;
+        else
+            flightControllerSinglePlayer.spinMultiplier = GetComponent<Slider>().value;
+
+        spinText.text = GetComponent<Slider>().value.ToString("F1");
+    }
+
+}
