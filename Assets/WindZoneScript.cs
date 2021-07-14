08d223832912ad24f4fb8dade1da9eba605b8206
Uncle Button <UncleButton@users.noreply.github.com>
2021-07-14 17:25:34 -0400

    Add drone prefab and wind

---
+++ Assets/WindZoneScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WindZoneScript : MonoBehaviour
+{
+    public Transform drone;
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag("Player") && other.attachedRigidbody!= null)
+        {
+            other.attachedRigidbody.AddForce((other.transform.position - drone.transform.position)*5 + new Vector3(Random.Range(-5f, 5f), Random.Range(-1f, 1f), Random.Range(-5f, 5f)));
+        }
+    }
+}
---
+++ Assets/underSparksScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class underSparksScript : MonoBehaviour
+{
+    void OnCollisionStay(Collision collision)
+    {
+        foreach (ContactPoint contact in collision.contacts)
+        {
+            print(contact.thisCollider.name + " hit " + contact.otherCollider.name);
+            // Visualize the contact point
+            Debug.DrawRay(contact.point, contact.normal, Color.red);
+        }
+        
+    }
+}
